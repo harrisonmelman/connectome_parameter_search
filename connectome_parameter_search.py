c15d7724d4580108e4df40468dcc88632807277b
Harrison Mansour <harrisonmelman@users.noreply.github.com>
2024-09-20 12:11:32 -0400

    add completion checking so trk and connectome generation are not re-ran. Can safely restart a run and not reprocess all lines

--- connectome_parameter_search.py
+++ connectome_parameter_search.py
@@ -13,11 +13,13 @@ import pandas as pd
 import os
 import numpy as np
 import nibabel as nib
+import glob
+import json
 DSI_STUDIO = "K:/CIVM_APPS/dsi_studio_64/dsi_studio_win_cpu_v2024-08-14/dsi_studio.exe"
+debug = False
 
 
-def main():
-    project_code = "20.5xfad.01"
+def main(project_code, runno_list):
     # need this to grab the label set
     archive_dir = "A:/{}/research".format(project_code)
 
@@ -32,14 +34,17 @@ def main():
 
     csv_table = pd.read_csv(experiment_table_path, header=1, delimiter="\t")
     experiment_list = csv_table.to_dict(orient='records')
-
-    runno_list = ["N59128NLSAM"]
+    count = 0
     for experiment in experiment_list:
+        if debug:
+            count+=1
         # load in the parameter set from CSV file
         # an experiment is one line of the CSV
         experiment_dir = "{}/{}".format(output_dir_base, experiment["uid"])
+        # save our experiment dict as a json in the experiment_dir
         if not os.path.exists(experiment_dir):
             os.makedirs(experiment_dir)
+        process_list = []
         for runno in runno_list:
             output_dir = "{}/{}".format(experiment_dir, runno)
             if not os.path.exists(output_dir):
@@ -48,12 +53,25 @@ def main():
             fib_file = "{}/nii4D_{}.src.gz.gqi.0.9.fib.gz".format(fib_dir, runno)
             qa_file = "{}/nii4D_{}.src.gz.gqi.0.9.fib.gz.qa.nii.gz".format(fib_dir, runno)
 
-            # formulate dsi studio call
-            setup_dsi_studio_trk_call(experiment, fib_file, qa_file, label_file, output_dir)
+            # check for completion by looking for any trk file in the outputs folder
+            found_trk = glob.glob("{}/*.trk.gz".format(output_dir))
+            found_tt = glob.glob("{}/*.tt.gz".format(output_dir))
+            if len(found_trk) > 0 or len(found_tt) > 0:
+                print("work already complete for experiment {} runno {}".format(experiment["uid"], runno))
+                continue
+            #if count > 4:
+            #    exit()
 
-            # use subprocess to run dsi studio
-            pass
-        pass
+            # formulate dsi studio call
+            cmd = setup_dsi_studio_trk_call(experiment, fib_file, qa_file, label_file, output_dir)
+            # run blocks until completion
+            subprocess.run(cmd, shell=True)
+            #p = subprocess.Popen(cmd)
+            #process_list.append(p)
+
+        with open('{}/experiment.json'.format(experiment_dir), 'w') as fp:
+            json.dump(experiment, fp)
+        exit_codes = [p.wait() for p in process_list]
 
 
 def find_vol_pct_threshold(image, percent):
@@ -67,8 +85,6 @@ def find_vol_pct_threshold(image, percent):
     return threshold
 
 
-
-# TODO: this does not yet handle FILEPATHS
 def setup_dsi_studio_trk_call(experiment: dict, fib_file, qa_nifti_file, label_file, output_dir):
     """takes in a set of DSI_Studio action=trk paramters and returns the command string"""
     options_str = ""
@@ -88,13 +104,28 @@ def setup_dsi_studio_trk_call(experiment: dict, fib_file, qa_nifti_file, label_f
     threshold = find_vol_pct_threshold(qa_nifti_file, experiment["fa_threshold"])
     options_str += "--fa_threshold={} ".format(threshold)
 
-    s = "{} {}".format(DSI_STUDIO, options_str)
-    print(s)
+    s = "{} --action=trk {}".format(DSI_STUDIO, options_str)
+    #print(s)
     return s
 
 
 if __name__ == "__main__":
-    main()
+    project_code = "20.5xfad.01"
+    runno_list = ['N59128NLSAM', 'N59130NLSAM', 'N59132NLSAM', 'N59134NLSAM', 'N60076NLSAM', 'N60145NLSAM',
+                  'N60149NLSAM', 'N60151NLSAM', 'N60153NLSAM', 'N60155NLSAM', 'N60165NLSAM', 'N60171NLSAM',
+                  'N60206NLSAM', 'N60208NLSAM', 'N60213NLSAM', 'N60215NLSAM', 'N60727NLSAM']
+    main(project_code, runno_list)
 
-    # create omni manova "dataframe"
-    # call omni manova code to run (MATLAB)
+
+    # I also need need need control data
+    # will use c57 black data @ 25um
+    project_code = "20.crater.01"
+    runno_list = ['N58680NLSAM', 'N58681NLSAM', 'N58682NLSAM', 'N58683NLSAM', 'N58684NLSAM', 'N58685NLSAM']
+
+    project_code = "21.QA94TAgilent.01"
+    runno_list = ['N60196NLSAM', 'N60197NLSAM', 'N60204NLSAM', 'N60205NLSAM', 'N60211NLSAM', 'N60217NLSAM',
+                  'N60218NLSAM', 'N60227NLSAM', 'N60228NLSAM', 'N60247NLSAM', 'N60248NLSAM']
+
+
+
+
